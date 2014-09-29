01deed8835b672a01a92486ae0f3e10e17b4d0b7
s.111chikiii <s.111chikiii@gmail.com>
2014-09-29 20:58:06 +0900

    URL生成

--- js/src/lp.js
+++ js/src/lp.js
@@ -6,5 +6,25 @@ $(function(){
 
   sync();
 
+
+  $(".p-lp__form__button").click(function() {
+    createRoom();
+  });
+
+  var createRoom = function() {
+    var roomId = $(".p-lp__form__input").val();
+    if (roomId.match(/^(room|js|css|assets|img|images|inquiry|demos)$/)) {
+      roomId = rand_url;
+    }
+    if (roomId == '') {
+      roomId = rand_url;
+    }
+    if (!roomId.match(/^[a-zA-Z1-9]+$/)) {
+      roomId = rand_url;
+    }
+    location.href = location.protocol + '//' + window.location.host + '/' + roomId;
+  }
+
+
   window.addEventListener("resize", sync, false);
 });
