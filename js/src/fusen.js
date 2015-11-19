d68439dd419c36503bf2914889691c3aa65119bc
shogochiai <shogo1104@gmail.com>
2015-11-19 14:40:35 +0900

    fav added

--- js/src/fusen.js
+++ js/src/fusen.js
@@ -6,21 +6,17 @@
 		this.ds = ds;
     this.pos = {x : 0, y : 0};
 		this.text = text;
+		this.linkedText = fusen_util.linknize( fusen_util.htmlEscape(text) );
 		this.color = color;
 		this.canvas = canvas;
 		this.fusenNumber = fusenNumber;
 		this.$el_fusen = $("#"+self.id);
 
-		var escapedText = fusen_util.htmlEscape(text);
-
-		// URLをリンク化
-		this.linkedText = fusen_util.linknize(escapedText);
-
 		// 削除ボタンのレイアウト等
     if (fusen_util.getDevice() == "mobile") {
-      this.canvas.append('<div id="'+this.id+'" class="p-husen theme-husen--'+color+'" data-fusenNumber="'+self.fusenNumber+'"><span>'+self.linkedText+'</span><input type="text" style="display:none;" /></div>');
+      this.canvas.append( self.mobilehtml() );
     } else {
-      this.canvas.append('<div id="'+this.id+'" class="p-husen theme-husen--'+color+'" data-fusenNumber="'+self.fusenNumber+'"><span>'+self.linkedText+'</span><input type="text" style="display:none;" /><div class="p-husen__cross">×</div></div>');
+      this.canvas.append( self.pchtml() );
     }
 
 		// 色を深める
@@ -28,6 +24,8 @@
 
 		// 削除ボタン
     var cross = $(".p-husen__cross", "#"+this.id);
+
+		self.favoriteListener();
 
 		// ドラッグボタン
     $("#"+self.id).draggable({
@@ -57,31 +55,34 @@
 		// 付箋クリック時の編集処理(タッチ端末未実装)
 		// 「再生」後の付箋は不可っぽい
 		$("#"+self.id).click(function(e){
-			$__fusen = $(this);
-			var text = $__fusen.find("span").text();
-			$__fusen.find("span").hide();
-			$input = $__fusen.find("input");
-			$input.val(text);
+			e.stopPropagation();
+			$self = $(this);
+			var when_clicked_text = $self.find("span").text();
+			$self.find("span").hide();
+			$input = $self.find("input");
+			$input.val(when_clicked_text);
 			$input.css("display", "inline");
 			$input.focus();
 			$input.off("keyup").on("keyup", function(e){
+				$_input = $(this);
+				var when_entered_text = $_input.val();
 
 				// ENTER
 				if(e.which == 13){
-					self.ds.set(self.id+"", {text: text},function(err, datum){
-						hideInput(text);
+					self.ds.set(self.id+"", {text: when_entered_text},function(err, datum){
+						hideInput(when_entered_text);
 					});
 				}
 
 				// ESC
        	if (e.which == 27) {
-					hideInput(text);
+					hideInput(when_clicked_text);
 				}
 
 				function hideInput(text) {
 					$input.hide();
-					$__fusen.find("span").text(text);
-					$__fusen.find("span").show();
+					$self.find("span").text(text);
+					$self.find("span").show();
 				}
 			});
 
@@ -126,9 +127,11 @@
 	}
 
 	Fusen.prototype.setText = function(text) {
+		this.text = text;
+		this.linkedText = fusen_util.linknize( fusen_util.htmlEscape(text) );
 		var self = this;
-		self.text = text;
-		$("#" + this.id).find("span").text(self.text);
+
+		$("#" + this.id).find("span").html(self.linkedText);
 	}
 
 	Fusen.prototype.removeSelf = function() {
@@ -146,6 +149,68 @@
 		var new_color = chroma.scale([darker_color, brighter_color])(ratio).hex();
 		self.$el_fusen.css("color", new_color);
 		self.$el_fusen.css("border-color", new_color);
+	}
+
+	Fusen.prototype.pchtml = function(){
+		var self = this;
+		return ''+
+			'<div id="'+self.id+'"'+
+				'class="p-husen theme-husen--'+self.color+'"'+
+				'data-fusenNumber="'+self.fusenNumber+'">'+
+				'<span>'+self.linkedText+'</span>'+
+				'<input type="text" style="display:none;" />'+
+				'<div class="p-husen__cross">×</div>'+
+				'<div class="p-husen__star-ever">★</div>'+
+			'</div>';
+	}
+
+	Fusen.prototype.favoriteListener = function () {
+		var self = this;
+		var ds_star = milkcocoa.dataStore("fusen-stars__"+self.id);
+		ds_star.stream().size(999).next(function(err, data) {
+			$("#"+self.id).find(".p-husen__star-ever").text("★"+data.length);
+		});
+
+
+		ds_star.on("push", function(data){
+			var $star = $("#"+self.id).find(".p-husen__star-pushed");
+			var num = parseInt( $star.text().substr(1) );
+			$star.text("★"+(num+1));
+		});
+
+		ds_star.on("remove", function(data){
+			var $star = $("#"+self.id).find(".p-husen__star-ever");
+			var num = parseInt( $star.text().substr(1) );
+			$star.text("★"+(num-1));
+		});
+
+		$("#"+self.id).find(".p-husen__star-ever").off().click(function(e){
+			e.stopPropagation();
+			$(this).removeClass("p-husen__star-ever");
+			$(this).addClass("p-husen__star-pushed");
+			ds_star.push({});
+		});
+
+		$("#"+self.id).find(".p-husen__star-pushed").off().click(function(e){
+			e.stopPropagation();
+			$(this).removeClass("p-husen__star-pushed");
+			$(this).addClass("p-husen__star-ever");
+			ds_star.stream().size(1).next(function(err, data){
+				ds_star.remove(data[0].id);
+			});
+		});
+
+	}
+
+	Fusen.prototype.mobilehtml = function(){
+		var self = this;
+		return ''+
+			'<div id="'+self.id+'"'+
+				'class="p-husen theme-husen--'+self.color+'"'+
+				'data-fusenNumber="'+self.fusenNumber+'">'+
+				'<span>'+self.linkedText+'</span>'+
+				'<input type="text" style="display:none;" />'+
+			'</div>';
 	}
 
 	global.Fusen = Fusen;
